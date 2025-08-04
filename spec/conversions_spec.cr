f82ace8fcdc34022cac42cf5b4cdcd097dcf0e4e
Brandon McGinty <git@bmcginty.us>
2025-08-04 14:14:48 -0400

    use new EventLoop (still requires compiling with CRYSTAL_OPTS=-Devloop=libevent)

--- spec/conversions_spec.cr
+++ spec/conversions_spec.cr
@@ -1,24 +1,30 @@
-def dbeq(db,a)
-db.query("select $1::text,$2,$3::text",
-"start",a,"end") do |rs|
-rs.each do
-rs.read.should eq "start"
-rs.read.should eq a
-rs.read.should eq "end"
-end
-end
+def dbeq(db, a)
+  db.query("select $1::text,$2,$3::text",
+    "start", a, "end") do |rs|
+    rs.each do
+      rs.read.should eq "start"
+      t = rs.read
+      begin
+        t.should eq a
+      rescue e
+        puts "db #{t} != actual #{a}"
+        raise e
+      end
+      rs.read.should eq "end"
+    end
+  end
 end
 
-db=PG_DB
-i16=[1_i16,-1000_i16]
-i32=[1_i32,-123456_i32]
-i64=[1_i64,-12341234_i64]
-f32=[0.123_f32,2.345_f32]
-f64=[4.56789_f64,-0.0135_f64]
-bool=[true,false]
-string=["test","TESTing"]
-chars=['a','b']
-times=[Time.local(1789,1,2,12,34,56).to_utc,Time.local.to_utc]
+db = PG_DB
+i16 = [1_i16, -1000_i16]
+i32 = [1_i32, -123456_i32]
+i64 = [1_i64, -12341234_i64]
+f32 = [0.123_f32, 2.345_f32]
+f64 = [4.56789_f64, -0.0135_f64]
+bool = [true, false]
+string = ["test", "TESTing"]
+chars = ['a', 'b']
+times = [Time.local(1789, 1, 2, 12, 34, 56, nanosecond: 123000).to_utc, Time.local.to_utc.at_beginning_of_second]
 {% for w in %w(i16 i32 i64 f32 f64 bool string chars times) %}
 it "should convert {{w.id}}" do
 dbeq(db,{{w.id}}[0])
--- src/pg/result_set.cr
+++ src/pg/result_set.cr
@@ -1,209 +1,203 @@
 module PG
-class ResultSet < DB::ResultSet
-include IO::Evented
+  class ResultSet < DB::ResultSet
+    @query : String?
+    @statement : DB::Statement
+    @connection : DB::Connection
+    @tuple : LibPQ::Result?
+    @row_num = -1
+    @col_num = 0
+    @affected_rows : Int64?
+    @io : IO::FileDescriptor
+    @eof = false
+    @read_timeout : Time::Span? = nil
+    @write_timeout : Time::Span? = nil
 
-@query : String?
-@statement : DB::Statement
-@connection : DB::Connection
-@tuple : LibPQ::Result?
-@row_num=-1
-@col_num=0
-@affected_rows : Int64?
-@fd : Int32
-@eof=false
-@read_timeout : Time::Span? = nil
-@write_timeout : Time::Span? = nil
+    setter :flags
+    getter :col_num, :row_num, :fd
+    getter! :connection, :affected_rows
 
-setter :flags
-getter :col_num,:row_num,:fd
-getter! :connection,:affected_rows
+    def check_open
+      raise IO::Error.new("closed connection") if @closed
+    end
 
-def check_open
-raise IO::Error.new("closed connection") if @closed
-end
+    def initialize(@statement, @query)
+      super statement
+      @connection = statement.connection
+      @io = IO::FileDescriptor.new(fd: LibPQ.socket(@connection), blocking: false)
+      @tuple = get_tuple.not_nil!
+      gar = get_affected_rows
+      gar
+    end
 
-def initialize(@statement,@query)
-super statement
-@connection=statement.connection
-@fd = LibPQ.socket(@connection)
-@tuple=get_tuple.not_nil!
-gar=get_affected_rows
-gar
-end
+    def get_affected_rows
+      if @affected_rows
+        @affected_rows
+      else
+        p = LibPQ.cmd_tuples(row)
+        s = String.new(p)
+        if s == ""
+          t = 0_i64
+        else
+          t = s.to_i64
+        end
+        @affected_rows = t
+        t
+      end
+    end
 
-def get_affected_rows
-if @affected_rows
-@affected_rows
-else
-p=LibPQ.cmd_tuples(row)
-s=String.new(p)
-if s==""
-t=0_i64
-else
-t=s.to_i64
-end
-@affected_rows=t
-t
-end
-end
+    def row
+      @tuple.not_nil!
+    end
 
-def row
-@tuple.not_nil!
-end
+    def row_count : Int32
+      LibPQ.ntuples(row)
+    end
 
-def row_count : Int32
-LibPQ.ntuples(row)
-end
+    def next_column_index : Int32
+      @col_num
+    end
 
-def next_column_index : Int32
-@col_num
-end
+    def column_count : Int32
+      LibPQ.nfields(row)
+    end
 
-def column_count : Int32
-LibPQ.nfields(row)
-end
+    def column_name(index : Int) : String
+      String.new LibPQ.fname(row, index).dup
+    end
 
-def column_name(index : Int) : String
-String.new LibPQ.fname(row,index).dup
-end
+    def column_names
+      size = column_count
+      t = Array(String).new(size)
+      size.times do |idx|
+        t << column_name(idx)
+      end
+      t
+    end
 
-def column_names
-size=column_count
-t=Array(String).new(size)
-size.times do |idx|
-t << column_name(idx)
-end
-t
-end
+    def [](key)
+      @col_num = 0
+      @col_num = column_names.index(key).not_nil!
+      read
+    end
 
-def [](key)
-@col_num=0
-@col_num=column_names.index(key).not_nil!
-read
-end
+    def get_io
+      @col_num += 1
+      if LibPQ.getisnull(row, row_num, col_num - 1) == 1
+        return nil
+      end
+      size = LibPQ.getlength(row, row_num, col_num - 1)
+      value = LibPQ.getvalue(row, row_num, col_num - 1)
+      IO::Memory.new(value.to_slice(size), writeable: false)
+    end
 
-def get_io
-@col_num+=1
-if LibPQ.getisnull(row,row_num,col_num-1)==1
-return nil
-end
-size=LibPQ.getlength(row,row_num,col_num-1)
-value=LibPQ.getvalue(row,row_num,col_num-1)
-IO::Memory.new(value.to_slice(size),writeable: false)
-end
+    def st
+      LibPQ.result_status(@tuple.not_nil!).to_s
+    end
 
-def st
-LibPQ.result_status(@tuple.not_nil!).to_s
-end
+    def stc
+      String.new LibPQ.cmd_status(@tuple.not_nil!)
+    end
 
-def stc
-String.new LibPQ.cmd_status(@tuple.not_nil!)
-end
+    def read
+      typ = LibPQ.ftype(row, col_num)
+      begin
+        t = Types.oids_to_crystal_classes[typ]
+      rescue e
+        raise DB::Error.new("pg type #{typ} not supported")
+      end
+      case t
+      when PG::Types::Converter
+        read_without_converter t.type
+      else
+        read_without_converter t
+      end
+    end
 
-def read
-typ=LibPQ.ftype(row,col_num)
-begin
-t=Types.oids_to_crystal_classes[typ]
-rescue e
-raise DB::Error.new("pg type #{typ} not supported")
-end
-case t
-when PG::Types::Converter
-read_without_converter t.type
-else
-read_without_converter t
-end
-end
+    def read_without_converter(type)
+      io = get_io
+      return nil unless io
+      type.from_pg io.not_nil!
+    end
 
-def read_without_converter(type)
-io=get_io
-return nil unless io
-type.from_pg io.not_nil!
-end
+    def read(type)
+      t = PG::Types.cr_pg_converter(type)
+      read_without_converter t
+    end
 
-def read(type)
-t=PG::Types.cr_pg_converter(type)
-read_without_converter t
-end
+    protected def do_close
+      super
+      while move_next
+      end
+      LibPQ.clear @tuple.not_nil!
+      @tuple = nil
+      # @io.close
+    end
 
-protected def do_close
-super
-while move_next
-end
-LibPQ.clear @tuple.not_nil!
-@tuple=nil
-close_events
-end
+    def move_next : Bool
+      begin
+        t = mmove_next
+        return t
+      rescue e
+        raise e
+      end
+    end
 
-def move_next : Bool
-begin
-t=mmove_next
-return t
-rescue e
-raise e
-end
-end
+    def mmove_next
+      if @eof == true
+        return false
+      end
+      error = nil
+      ret = false
+      while 1
+        if row_num < (row_count - 1) && row_count > 0
+          @row_num += 1
+          @col_num = 0
+          ret = true
+          break
+        end
+        t = get_tuple
+        if t.null?
+          @eof = true
+          ret = false
+          break
+        end
+        LibPQ.clear row
+        @tuple = t.not_nil!
+        @row_num = -1
+      end
+      handle_error
+      ret
+    end
 
-def mmove_next
-if @eof == true
-return false
-end
-error=nil
-ret=false
-while 1
-if row_num < (row_count-1) && row_count > 0
-@row_num+=1
-@col_num=0
-ret=true
-break
-end
-t=get_tuple
-if t.null?
-@eof=true
-ret=false
-break
-end
-LibPQ.clear row
-@tuple=t.not_nil!
-@row_num=-1
-end
-handle_error
-ret
-end
+    def handle_error
+      st = LibPQ.result_status(row)
+      case st
+      when .bad_response?
+      when .fatal_error?
+        e = String.new(LibPQ.error_message(connection))
+        raise DB::Error.new(e)
+      end
+    end
 
-def handle_error
-st=LibPQ.result_status(row)
-case st
-when .bad_response?
-when .fatal_error?
-e=String.new(LibPQ.error_message(connection))
-raise DB::Error.new(e)
-end
-end
+    def get_tuple : LibPQ::Result
+      ret = nil
+      while 1
+        good = LibPQ.consume_input(@connection)
+        if good == 0
+          e = String.new LibPQ.error_message(connection)
+          raise DB::Error.new(e)
+        end
+        # handle_notifications
+        busy = LibPQ.is_busy(@connection)
+        if busy == 0
+          ret = LibPQ.get_result(@connection)
+          break
+        end
+        Crystal::EventLoop.current.wait_readable(@io)
+      end # while
+      ret.not_nil!
+    end # def
 
-def get_tuple : LibPQ::Result
-ret=nil
-while 1
-good=LibPQ.consume_input(@connection)
-if good==0
-e=String.new LibPQ.error_message(connection)
-raise DB::Error.new(e)
-end
-#handle_notifications
-busy=LibPQ.is_busy(@connection)
-if busy==0
-ret=LibPQ.get_result(@connection)
-break
-end
-wait_readable
-end #while
-ret.not_nil!
-end #def
+  end # class
 
-def close_events
-evented_close
-end
-
-end #class
-
-end #module
+end # module
